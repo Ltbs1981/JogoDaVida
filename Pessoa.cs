e616d138d7a03b03970f25b14c80f3fee814576f
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-04-23 00:13:29 -0300

    Iniciada a classePessoa

--- Pessoa.cs
+++ Pessoa.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JogoDaVida
 {
     internal class Pessoa
     {
+        // Propriedades auto-implementadas para nome e idade
+        public string Nome { get; private set; }
+        public int Idade { get; private set; }
+
+        // Construtor para inicializar o objeto com nome e idade
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        // Métodos para configurar o nome e a idade
+        public void SetNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public void SetIdade(int idade)
+        {
+            Idade = idade;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace JogoDaVida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciar um objeto Pessoa
+            Pessoa pessoa = new Pessoa("", 0);
+
+            // Input para o nome
+            Console.Write("Digite o nome da pessoa: ");
+            string nome = Console.ReadLine();
+
+            // Input para a idade
+            Console.Write("Digite a idade da pessoa: ");
+            int idade;
+            while (!int.TryParse(Console.ReadLine(), out idade) || idade <= 0)
+            {
+                Console.WriteLine("Idade inválida. Por favor, digite novamente.");
+                Console.Write("Digite a idade da pessoa: ");
+            }
+
+            // Configurar o nome e a idade da pessoa usando os métodos SetNome e SetIdade
+            pessoa.SetNome(nome);
+            pessoa.SetIdade(idade);
+
+            // Imprimir os dados da pessoa
+            Console.WriteLine("Dados da pessoa:");
+            Console.WriteLine("Nome: " + pessoa.Nome);
+            Console.WriteLine("Idade: " + pessoa.Idade);
+        }
+    }
+}
